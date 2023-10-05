57144b81d3f7e8a1f0149b411b077e83c36d0433
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-10-05 11:12:34 +0300

    added env file

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,8 +1,19 @@
+import os
 from flask import Flask
-from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
 
 app = Flask(__name__)
 
-api = Api(app)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
 
 from api import routes
